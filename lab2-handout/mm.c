c1ccdda24cabf92c901ce3f0f312f7fb55d16f74
Joe Dunton <duntonjoe@gmail.com>
2019-02-25 16:10:39 -0500

    write a function to find the first place a new block will fit, write a second funtion that places a block at that given address

--- lab2-handout/mm.c
+++ lab2-handout/mm.c
@@ -64,6 +64,57 @@ toggleBlock (address targetAddr){
 	return targetAddr ^ 0x1;
 }
 
+static void *extend_heap(size_t words)
+{
+ char *bp;
+ size_t size;
+
+ /* Allocate an even number of words to maintain alignment */
+ size = (words % 2) ? (words+1) * WSIZE : words * WSIZE;
+ if ((long)(bp = mem_sbrk(size)) == -1)
+ return NULL;
+
+ /* Initialize free block header/footer and the epilogue header */
+ makeBlock(header(bp) size, 0); /* Free block header */
+ makeBlock(footer(bp), size, 0);  /* Free block footer */
+ makeBlock(header(nextBlock(bp)), 0, 1); /* New epilogue header */
+ 
+ /* Coalesce if the previous block was free */
+ return coalesce(bp);
+}
+
+static void *coalesce(void *bp)
+ {
+ size_t prev_alloc = isAllocated(footer(prevBlock(bp)));
+ size_t next_alloc = isAllocated(header(nextBlock(bp)));
+ size_t size = sizeOf(header(bp));
+
+ if (prev_alloc && next_alloc) { /* Case 1 */
+ return bp;
+ }
+
+ else if (prev_alloc && !next_alloc) { /* Case 2 */
+ size += sizeOf(header(nextBlock(bp)));
+ makeBlock(header(bp), size, 0);
+ makeBlock(footer(bp) size, 0);
+ }
+
+ else if (!prev_alloc && next_alloc) { /* Case 3 */
+ size += sizeOf(header(prevBlock(bp)));
+ makeBlock(footer(bp, size, 0);
+ makeBlock(header(prevBlock(bp)), size, 0);
+ bp = prevBlock(bp);
+ }
+
+ else { /* Case 4 */
+ size += sizeOf(header(prevBlock(bp))) + sizeOf(footer(nextBlock(bp)));
+ makeBlock(header(prevBlock(bp)), size, 0);
+ makeBlock(footer(nextBlock(bp)), size, 0);
+ bp = prevBlock(bp);
+ }
+ return bp;
+}
+
 int
 mm_init (void)
 {
@@ -89,8 +140,35 @@ mm_init (void)
 void*
 mm_malloc (uint32_t size)
 {
-  fprintf(stderr, "allocate block of size %u\n", size);
-  return NULL;
+ size_t asize; /* Adjusted block size */
+ size_t extendsize; /* Amount to extend heap if no fit */
+ char *bp;
+
+ /* Ignore spurious requests */
+ if (size == 0)
+ return NULL;
+
+ /* Adjust block size to include overhead and alignment reqs. */
+ if (size <= DSIZE)
+ 	asize = 2*DSIZE;
+ else
+	 asize = DSIZE * ((size + (DSIZE) + (DSIZE-1)) / DSIZE);
+
+ /* Search the free list for a fit */
+ if ((bp = find_fit(asize)) != NULL) {
+	 place(bp, asize);
+ 	return bp;
+ }
+
+ /* No fit found. Get more memory and place the block */
+ extendsize = MAX(asize,CHUNKSIZE);
+ if ((bp = extend_heap(extendsize/WSIZE)) == NULL)
+ 	return NULL;
+ place(bp, asize);
+ return bp;
+
+ //fprintf(stderr, "allocate block of size %u\n", size);
+ //return NULL;
 }
 
 void
