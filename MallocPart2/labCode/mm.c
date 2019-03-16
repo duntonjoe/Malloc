409d4c16419dbf52138e358f693ee7fd271b0b89
Jimmy Roche <jimmyroche@Jimmys-MacBook-Pro.local>
2019-03-16 17:50:31 -0400

    blocksFromBytes might not work I'm sorry pt 3

--- MallocPart2/labCode/mm.c
+++ MallocPart2/labCode/mm.c
@@ -141,7 +141,7 @@ static inline address coalesce(address bp)
  * 		bytes -> represents the number of bytes requested by the application
  */
 static inline uint32_t blocksFromBytes (uint32_t bytes) {
-	u_int32_t blocks = bytes / 8;
+	uint32_t blocks = bytes / 8;
 	if (bytes % 16 == 0 || bytes % 16 > 8){
 		blocks++;
 	}
